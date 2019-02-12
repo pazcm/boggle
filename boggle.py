8d96c0f9674e4ebc452daebf9e6fe3d6e14b985c
Marta Paz <pazmarta@gmail.com>
2019-02-12 14:51:37 +0000

    create a second dictionary structure for stems (partial words)

--- boggle.py
+++ boggle.py
@@ -60,10 +60,6 @@ def path_to_word(grid, path):
     return ''.join([grid[p] for p in path])
 
 
-def word_in_dictionary(word, dict):
-    return word in dict
-
-
 def search(grid, dictionary):
     """
     Search thrugh the paths to locate words by matching
@@ -71,11 +67,14 @@ def search(grid, dictionary):
     """
     neighbours = all_grid_neighbours(grid)
     paths = []
+    full_words, stems = dictionary
 
     def do_search(path):
         word = path_to_word(grid, path)
-        if word_in_dictionary(word, dictionary):
+        if word in full_words:
             paths.append(path)
+        if word not in stems:
+            return
         for next_pos in neighbours[path[-1]]:
             if next_pos not in path:
                 do_search(path + [next_pos])
@@ -93,15 +92,23 @@ def get_dictionary(dictionary_file):
     """
     Load dictionary file
     """
+    full_words, stems = set(), set()
     with open(dictionary_file) as f:
-        return [w.strip().upper() for w in f]
+        for word in f:
+            word = word.strip().upper()
+            full_words.add(word)
+
+            for i in range(1, len(word)):
+                stems.add(word[:i])
+
+        return full_words, stems
 
 
 def main():
     """
     This is the function that will run the whole project
     """
-    grid = make_grid(3, 3)
+    grid = make_grid(4, 4)
     dictionary = get_dictionary('words.txt')
     words = search(grid, dictionary)
     for word in words:
@@ -110,4 +117,4 @@ def main():
 
 
 if __name__ == "__main__":
-main()
+    main()
