622fe0385ad71adfa3b92b8f6e2a28faf2eddfc1
Marta Paz <pazmarta@gmail.com>
2019-02-12 12:04:30 +0000

    test boggle grid filled with letters

--- boggle.py
+++ boggle.py
@@ -1,7 +1,12 @@
+from string import ascii_uppercase
+from random import choice
+
+
 def make_grid(width, height):
     """
     Creates a grid that will hold all of the tiles
-    for a boggle game | creates a dictionary with the row column tuple as the key and space as the value, (no letters yet).
+    for a boggle game
     """
-    return {(row, col): ' ' for row in range(height)
+    return {(row, col): choice(ascii_uppercase)
+        for row in range(height)
 for col in range(width)}
