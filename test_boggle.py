d2d5dcf9084ba1c24b26fae324f7bc5d3d02f70b
Marta Paz <pazmarta@gmail.com>
2019-02-12 11:40:06 +0000

    test if a given position row and columns is in the grid

--- test_boggle.py
+++ test_boggle.py
@@ -22,4 +22,14 @@ class TestBoggle(unittest.TestCase):
         grid = boggle.make_grid(2, 3)
         self.assertEqual(len(grid), 6)
     
-   
+        """
+        Test to ensure that all the coordinates 
+        inside the grid can be accessed | check if a given position row and columns is in the grid
+        """
+    def test_grid_coordinates(self):
+        grid = boggle.make_grid(2, 2)
+        self.assertIn((0, 0), grid)
+        self.assertIn((0, 1), grid)
+        self.assertIn((1, 0), grid)
+        self.assertIn((1, 1), grid)
+        self.assertNotIn((2, 2), grid)
